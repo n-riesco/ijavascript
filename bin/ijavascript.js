d97f15326870d06da6e2d6fd3374e29646ff90ef
Nicolas Riesco <enquiries@nicolasriesco.net>
2015-02-03 10:50:27 +0000

    Change license
    Changed license to BSD 3-Clause.
    
    I have to admit that when I published IJavascript I didn't give enough
    thought to contributions by others. I'm concerned a BSD 2-Clause license
    may discourage some contributors and for this reason I have decided to
    move the project to a BSD 3-Clause license before I accept any
    contributions by others.

--- bin/ijavascript.js
+++ bin/ijavascript.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 /*
- * BSD 2-Clause License
+ * BSD 3-Clause License
  *
- * Copyright (c) 2015, Nicolas Riesco
+ * Copyright (c) 2015, Nicolas Riesco and others as credited in the AUTHORS file
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -15,6 +15,10 @@
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  * this list of conditions and the following disclaimer in the documentation
  * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE

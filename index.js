4595311318150456c72f9e19992eb995584746b9
Nicolas Riesco <enquiries@nicolasriesco.net>
2017-03-24 18:27:36 +0000

    index.js: Add warning
    * Added warning against use of `require("ijavascript")`. This is only a
      hack to support https://github.com/nteract/nteract/pull/1339 .
    
    * Recommeded the use of `require("jp-kernel")` instead.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
+// Do not `require("ijavascipt")`
+// Please `require("jp-kernel")` instead
+// This is only a hack to support https://github.com/nteract/nteract/pull/1339
 module.exports = require("jp-kernel");
